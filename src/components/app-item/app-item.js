bf181ff468bc05ad30b735efc9245ce93701fb93
coldfear457@gmail.com <coldfear457@gmail.com>
2021-12-28 09:35:21 +0600

    feat: add cont currency at app-item

--- src/components/app-item/app-item.js
+++ src/components/app-item/app-item.js
@@ -1,13 +1,14 @@
 import './app-item.css';
 
 const AppItem = ({name, rent, src, classify, transmission}) =>{
+  const currency = ' €';
 
     return(
       <div className="add-item">
         <h3 className="add-item-title">{name}</h3>
         <p className="add-item-characters">{transmission}, {classify}</p>
         <img src={`/images/${src}.jpg`} alt={src}></img>
-        <span className="add-item-price">{rent + ' €'}</span>
+        <span className="add-item-price">{rent + currency}</span>
       </div>
 
     )
